b350af7fa6046075e49c1b50afabf28922ce4409
Salvador Holguin <sholguin@umich.edu>
2021-09-02 09:39:44 -0400

    init

---
+++ apps/info-all/pages/index.tsx
@@ -0,0 +1,15 @@
+import styles from './index.module.scss';
+
+export function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.scss file.
+   */
+  return (
+    <h1>Hello World</h1>
+    
+  );
+}
+
+export default Index;
